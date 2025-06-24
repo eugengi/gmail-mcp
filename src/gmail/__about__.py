1cb4fdc9738dc15c0a677fffbec72e9bade19ff2
Eugene Mwangi <mwangi.em37@gmail.com>
2025-06-24 12:18:19 +0300

    chore(init): Scaffold project base with `hatch`
    Use hatch to scaffold project with common defaults.
    
    [Commands]
    ➜ hatch new "gmail-mcp"
    
    [Note]
    - The output of the above cmd was integrated with the remote checkout.
    - Minor tweaks were made to hatch output.

---
+++ src/gmail/__about__.py
@@ -0,0 +1,4 @@
+# SPDX-FileCopyrightText: 2025-present Eugene Mwangi <mwangi.em37@gmail.com>
+#
+# SPDX-License-Identifier: MIT
+__version__ = "0.0.1"
---
+++ src/gmail/__init__.py
@@ -0,0 +1,3 @@
+# SPDX-FileCopyrightText: 2025-present Eugene Mwangi <mwangi.em37@gmail.com>
+#
+# SPDX-License-Identifier: MIT
